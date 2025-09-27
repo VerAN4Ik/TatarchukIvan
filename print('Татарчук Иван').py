5592b3b53feadca7c5090a58090cf195551aec15
VerAN4Ik <veranik701@gmail.com>
2025-09-27 14:46:30 +0300

    Add files via upload

---
+++ print('Татарчук Иван').py
@@ -0,0 +1,18 @@
+#print('Татарчук Иван')
+#print(15)
+#print(3.14159)
+#print("True")
+
+
+
+
+#a = input()
+#b = int(input())
+#print("Привет, я люблю", a, "Цвет и программирую уже", b, "лет")
+
+#print("2025/09/13")
+#print("Python_is great")
+
+n = int(input())
+x = input()
+print(type(x), type(n))
