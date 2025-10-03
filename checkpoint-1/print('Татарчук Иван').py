c0f8f28ba7a5f11fe024faa2b0a251b1449652f4
VerAN4Ik <veranik701@gmail.com>
2025-10-03 17:04:31 +0300

    Add files via upload

---
+++ checkpoint-1/print('Татарчук Иван').py
@@ -0,0 +1,23 @@
+#print('Татарчук Иван') str
+#print(15) int
+#print(3.14159) float
+#print(True) boot
+
+#a = input()
+#b = int(input())
+#print("Привет, я люблю", a, "Цвет и программирую уже", b, "лет")
+
+#print("2025", "09","13", sep="/")
+#print("Python", end="_") 
+#print("is great", sep=" ")
+
+#userName = input()
+#userAge = int(input())
+#print(type(userName).__name__) # output: str
+#print(type(userAge).__name__) # output: int
+
+
+print("Преобразование пустой строки:", bool(""))       #False
+print("Преобразование строки с пробелом:", bool(" "))    
+print("Преобразование числа 0:", bool(0))                 
+print("Преобразование числа -10:", bool(-10)) 
